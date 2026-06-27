22c10a34b30ee29d1d7e06736e7539e8484d1fcc
Chris-846 <yy846@yonsei.ac.kr>
2026-06-27 20:09:11 +0900

    Update token_cache.py

--- token_cache.py
+++ token_cache.py
@@ -1,36 +1,29 @@
 import json
 import requests
 
-def maketoken(dist = "REAL"):
+import json
+import requests
 
-    global stock_info
-    
-    headers = {"content-type":"application/json"}
+def maketoken(config, dist="REAL"):
+    headers = {"content-type": "application/json"}
     body = {
-        "grant_type":"client_credentials",
-        "appkey":stock_info['REAL_APP_KEY'], 
-        "appsecret":stock_info['REAL_APP_SECRET']
-        }
+        "grant_type": "client_credentials",
+        "appkey": config.app_key,
+        "appsecret": config.app_secret
+    }
 
-    # 한국투자증권에 Request
     PATH = "oauth2/tokenP"
-    URL = f"{stock_info['REAL_URL']}/{PATH}"
+    URL = f"{config.api_domain}/{PATH}"
     res = requests.post(URL, headers=headers, data=json.dumps(body))
-    
+
     if res.status_code == 200:
-        my_token = res.json()["access_token"]
+        my_token = res.json().get("access_token")
 
-        datadict = dict()
-
-        # 해당 토큰을 작업 공간에 저장
-        datadict["authorization"] = my_token
+        datadict = {"authorization": my_token}
         with open('./stock_token.json', 'w') as outfile:
-            json.dump(datadict, outfile)   
+            json.dump(datadict, outfile)
 
-        print("My TOKEN : ", my_token)
+        print("My TOKEN:", my_token)
         return my_token
-
     else:
         raise Exception('status code not 200 & Token Auth Fail!')
-
-{"date": "2024-06-18", "access_token": "", "expires_in": 3600}

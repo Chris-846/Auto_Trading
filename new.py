33ce7e79154e716eb401fd4c206f495780d82e30
Chris-846 <yy846@yonsei.ac.kr>
2026-06-27 21:25:52 +0900

    Update new.py

--- new.py
+++ new.py
@@ -8,13 +8,13 @@ def maketoken(dist = "REAL"):
     headers = {"content-type":"application/json"}
     body = {
         "grant_type":"client_credentials",
-        "appkey":stock_info["GH_APPKEY"], 
-        "appsecret":stock_info["GH_APPSECRET"]
+        "appkey":stock_info['GH_APPKEY'], 
+        "appsecret":stock_info['GH_APPSECRET']
         }
 
     # 한국투자증권에 Request
     PATH = "oauth2/tokenP"
-    URL = f"{stock_info["GH_API_DOMAIN"]}/{PATH}"
+    URL = f"{stock_info['GH_API_DOMAIN']}/{PATH}"
     res = requests.post(URL, headers=headers, data=json.dumps(body))
     
     if res.status_code == 200:
